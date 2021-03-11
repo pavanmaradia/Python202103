9b386438f6b71ddc711a03351804dd0249a93a2d
parthpatel <pavan.maradia@yahoo.com>
2021-03-11 08:09:14 +0530

    Session 06: completed with variable and datatype

---
+++ sessions/session_06.py
@@ -0,0 +1,49 @@
+"""
+Variables
+Datatype
+    int
+    str
+    boolean
+    float
+"""
+#
+# var_number = 1
+# var_string = "Test"
+# var_number_2 = 1
+# var_number_2 = 'test'
+#
+#
+# print(F"var_number: {var_number}: {id(var_number)}")
+# print("var_string: {var} {var_id}".format(var=var_string, var_id=id(var_string)))
+# print("var_number_2: " + var_number_2 + ": " + str(id(var_number_2)))
+#
+# print("{:20}".format(2))
+
+# single line comment
+'''
+Multiline comment
+'''
+"""
+Multiline comment
+"""
+
+var_int = -1
+# print(F"var_int : {var_int} : {type(var_int)} : {id(var_int)}")
+# var_float = 10.22
+# print(F"var_float : {var_float} : {type(var_float)} : {id(var_float)}")
+var_str = '''This is a test string\nThis is another string'''
+var_str = '0123456789'
+# print(F"var_str : {var_str} : {type(var_str)} : {id(var_str)}")
+# var_boolean = True
+# print(F"var_boolean : {var_boolean} : {type(var_boolean)} : {id(var_boolean)}")
+
+
+# print(var_str.split("\n"))
+# print(var_str.count('is'))
+# print(var_str.title())
+# print(var_str.capitalize())
+# print(var_str.lower())
+# print(var_str.upper())
+# print(var_str[0:10:3])
+# print(var_str[::-1])
+
