b401a8298443d793610feb40f443385f0a91d5ff
parthpatel <pavan.maradia@yahoo.com>
2021-03-12 09:01:58 +0530

    Session 08: completed with Data Structure set and array. Conditional statement

---
+++ sessions/session_08.py
@@ -0,0 +1,90 @@
+"""
+Data Structure
+    set
+    array
+Conditional Statement
+    if
+    if else
+    if elif else
+    nested if
+
+Assignment:
+    1) find the union from 3 sets
+    2) find the difference from 3 sets
+    3) find the maximum number from 5 variables
+    4) take input from user and if input is 'a' then print 'apple' like wise create a to g
+"""
+
+# var_dict = {'key1': 'value1'}
+#
+# var_dict['key1'] = 1
+# print(var_dict)
+# var_set_1 = {1, 2, 3, 4, 5, 6, 7, 8, 1, 1, 2, 2, 2, 2, 3, 'hi'}
+# var_set_2 = {1, 66, 33, 22, 8, 9, 'hi'}
+#
+# print(var_set_1.intersection(var_set_2))
+# print(var_set_2.difference(var_set_1))
+# print(var_set_2.union(var_set_1))
+
+# import array
+#
+# a = array.array('l', [1, 2, 3])
+#
+# print(a.append(8))
+# print(a)
+# b = array.array('u', 'This is my char string')
+# print(b[0])
+
+
+"""
+if <condition> <operator> <condition>:
+    <statement_1>
+    <statement_2>
+    if <condition>:
+        <statement_4>
+        if <condition>:
+            <statement_5>
+            if <condition>:
+                <statement_6>
+elif <condition>:
+    <statement_8>                
+else:
+    <statement_7>
+    
+<statement_3>
+"""
+# from datetime import datetime, timedelta
+#
+# no_1 = datetime.now()
+# no_2 = no_1 + timedelta(days=20)
+# print(no_1)
+# print(no_2)
+# no_1 = input("Enter Number 1: ")
+# no_2 = input("Enter Number 2: ")
+
+# no_1 = '10'
+# no_2 = 'Hi'
+
+# if no_2 < no_1:
+#     print(F'Number 1 ({no_1}) is maximum.')
+# elif no_1 == no_2:
+#     print(F"Number 1 ({no_1}) and Number 2 ({no_2}) are same.")
+# else:
+#     print(F'Number 2 ({no_2}) is maximum.')
+
+#
+# no_1 = 10
+# no_2 = 14
+# no_3 = 1
+#
+# if no_1 < no_2 and no_1 < no_3:
+#     print(F'Number 1 ({no_1}) is minimum.')
+#
+# elif no_2 < no_1 <= no_3 or no_2 < no_3 <= no_1:
+#     print(F'Number 2 ({no_2}) is minimum.')
+#
+# elif no_3 < no_1:
+#     if no_3 < no_2:
+#         print(F'Number 3 ({no_3}) is minimum.')
+#     else:
+#         print(F'Number 2 and Number 3 are same')
